291b19ff337b6df70ddfd797f599fe9bfe55b7c5
kanstantsin.zayats <kanstantsin.zayats@altoros.com>
2023-03-26 20:20:32 +0300

    Additing basic layout and global styles

---
+++ src/App.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+import Button, { ButtonTypes } from "./Components/Button";
+import Navigation from "./Components/Navigation/Navigation";
+import NewsListUnit from "./Components/NewsListUnit/NewsListUnit";
+import NewsList from "./Components/NewsList/NewsList";
+
+
+const App = () => {
+
+  return (
+    <div className='main-container'>
+
+      <div className="container">
+
+        
+      </div>
+
+    </div>
+  );
+};
+
+export default App;
