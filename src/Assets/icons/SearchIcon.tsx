3bcf6da8e5b176b960414606fa49fd4472dbd300
kanstantsin.zayats <kanstantsin.zayats@altoros.com>
2023-03-26 20:34:33 +0300

    Icon fix

--- src/Assets/icons/SearchIcon.tsx
+++ src/Assets/icons/SearchIcon.tsx
@@ -17,15 +17,15 @@ export const SearchIcon: FC<IconProps> = ({unicClass}) => {
       <path
         d="M11 18C14.866 18 18 14.866 18 11C18 7.13401 14.866 4 11 4C7.13401 4 4 7.13401 4 11C4 14.866 7.13401 18 11 18Z"
         stroke="#313037"
-        stroke-width="2"
-        stroke-linecap="round"
-        stroke-linejoin="round"/>
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"/>
       <path
         d="M20 20L16 16"
         stroke="#313037"
-        stroke-width="2"
-        stroke-linecap="round"
-        stroke-linejoin="round"/>
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"/>
     </svg>
   );
 };
