dbdff19667967388f8d5620e7488b077fbf7146b
kanstantsin.zayats <kanstantsin.zayats@altoros.com>
2023-03-28 20:44:45 +0300

    theme switcher scss

--- src/Components/ThemeSwitcher/ThemeSwitcher.tsx
+++ src/Components/ThemeSwitcher/ThemeSwitcher.tsx
@@ -7,16 +7,14 @@ const ThemeSwitcher = () => {
   const handleChange = (event: { target: { checked: any; }; }) => {
     if (event.target.checked) {
       document.body.classList.add("dark-theme");
-      console.log('✅ Checkbox is checked');
     } else {
       document.body.classList.remove("dark-theme");
-      console.log('⛔️ Checkbox is NOT checked');
     }
   };
 
   return (
     <label className={styles["theme-switcher"]} htmlFor="theme-switcher-input">
-      <input className={styles["theme-switcher-check"]} onChange={handleChange} id='theme-switcher-input' type="checkbox"/>
+      <input className={styles["theme-switcher-check"]} onChange={handleChange} id='theme-switcher-input' type='checkbox' />
       <div className={styles["theme-switcher-indicator"]} />
     </label>
   );
