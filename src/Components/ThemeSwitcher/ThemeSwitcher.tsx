eef01da94b63c26db782fb9c640a5494c4e54b67
kanstantsin.zayats <kanstantsin.zayats@altoros.com>
2023-03-31 19:14:45 +0300

    refactor theme switcher component with useState

--- src/Components/ThemeSwitcher/ThemeSwitcher.tsx
+++ src/Components/ThemeSwitcher/ThemeSwitcher.tsx
@@ -1,20 +1,22 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 import styles from './ThemeSwitcher.module.scss';
 
 const ThemeSwitcher = () => {
 
-  const handleChange = (event: { target: { checked: any; }; }) => {
-    if (event.target.checked) {
-      document.body.classList.add("dark-theme");
-    } else {
-      document.body.classList.remove("dark-theme");
-    }
-  };
+  const getBody = document.body;
+
+  const [checked, setChecked] = useState(false);
+
+  if(checked) {
+    getBody.classList.remove("dark-theme");
+  } else {
+    getBody.classList.add("dark-theme");
+  }
 
   return (
     <label className={styles["theme-switcher"]} htmlFor="theme-switcher-input">
-      <input className={styles["theme-switcher-check"]} onChange={handleChange} id='theme-switcher-input' type='checkbox' />
+      <input className={styles["theme-switcher-check"]} onChange={() => setChecked(!checked)} id='theme-switcher-input' type='checkbox' />
       <div className={styles["theme-switcher-indicator"]} />
     </label>
   );
