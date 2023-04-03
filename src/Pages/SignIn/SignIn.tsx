256f99b87429cd5222b88c56bd5f1ef29338cfc0
kanstantsin.zayats <kanstantsin.zayats@altoros.com>
2023-04-03 22:18:43 +0300

    Sign in page finished

--- src/Pages/SignIn/SignIn.tsx
+++ src/Pages/SignIn/SignIn.tsx
@@ -13,10 +13,14 @@ const SignIn = () => {
           <Input type={'text'} title={'Email'} placeholder={'Your email'} />
         </div>
         <div className={styles['registration-unit']}>
-          <Input type={'password'} title={'Password'} error={'eqweq'} placeholder={'Your pasword'} />
+          <Input type={'password'} title={'Password'} placeholder={'Your pasword'} />
         </div>
+        <div className={styles['forgot-password']}>Forgot password?</div>
         <div className={styles['registration-unit']}>
           <Button title={'Sign In'} type={ButtonTypes.Primary} />
+        </div>
+        <div className={styles['registration-unit-auth']}>
+          {'Don’t have an account?'} <span>{'Sign Up'}</span>
         </div>
       </>
     </FormContainer>
