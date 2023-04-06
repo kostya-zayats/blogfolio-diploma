f55bd55c85c102295367f117e4b4e94771bf39fc
kanstantsin.zayats <kanstantsin.zayats@altoros.com>
2023-04-06 19:32:15 +0300

    add Sign in link

--- src/Components/UserName/UserName.tsx
+++ src/Components/UserName/UserName.tsx
@@ -1,6 +1,8 @@
 import React, {FC} from 'react';
 
 import styles from './UserName.module.scss';
+import {NavLink} from "react-router-dom";
+import {PageNames} from "../../Constants/@types";
 
 type UserNameProps = {
   title: string;
@@ -8,11 +10,12 @@ type UserNameProps = {
 
 const UserName: FC<UserNameProps> = ({title}) => {
   const initials = title[0] + title[title.lastIndexOf(' ') + 1].toUpperCase();
+  const isLoggedIn = false;
 
   return (
     <div className={styles["nav-user"]}>
-      <div className={styles["nav-user-initials"]}>{initials}</div>
-      <div className={styles["nav-user-name"]}>{title}</div>
+      {isLoggedIn ? <div className={styles["nav-user-initials"]}>{initials}</div> : null}
+      {isLoggedIn ? <div className={styles["nav-user-name"]}>{title}</div> : <NavLink to={PageNames.SignIn}>Sign In</NavLink>}
     </div>
   );
 };
