f45f92a7865d61c58c8c3eed3d8e5274c4acd725
kanstantsin.zayats <kanstantsin.zayats@altoros.com>
2023-05-10 01:42:56 +0300

    post navigation

--- src/Pages/Post/Post.tsx
+++ src/Pages/Post/Post.tsx
@@ -6,6 +6,8 @@ import {useParams} from "react-router";
 import {useDispatch, useSelector} from "react-redux";
 import {getSingleNews} from "../../Redux/Reducers/newsReducer";
 import newsSelectors from "../../Redux/Selectors/newsSelectors";
+import {PageNames} from "../../Constants/@types";
+import {NavLink} from "react-router-dom";
 
 const Post = () => {
   const params = useParams();
@@ -21,11 +23,21 @@ const Post = () => {
 
   return news ? (
     <div className={styles['news-post-container']}>
-      <div>{news.id}</div>
+      <div className={styles['news-post-nav']}>
+        <div className={styles['news-post-nav-unit']}>
+          <NavLink className={styles['news-post-nav-unit-link']} to={PageNames.Home}>{'Home'}</NavLink>
+        </div>
+        <div className={styles['news-post-nav-separator']}>
+          /
+        </div>
+        <div className={styles['news-post-nav-unit']}>
+          Post {news.id}
+        </div>
+      </div>
       <Header title={news.title} />
 
       <div className={styles['news-post-img-container']}>
-        <img className={styles['news-post-img']} src={news.image} alt=""/>
+        <img className={styles['news-post-img']} src={news.image} alt={news.image} />
       </div>
       <div className={styles['news-post-text-container']}>
         <p>
