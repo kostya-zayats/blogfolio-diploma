7d1dd4e918c7c0f94cdb7c96fbc51f2abcf8224e
kanstantsin.zayats <kanstantsin.zayats@altoros.com>
2023-05-12 00:47:17 +0300

    log out popup visibility

--- src/Components/UserName/UserName.tsx
+++ src/Components/UserName/UserName.tsx
@@ -1,10 +1,11 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 import styles from './UserName.module.scss';
 import {NavLink} from "react-router-dom";
 import {PageNames} from "../../Constants/@types";
 import {useSelector} from "react-redux";
 import authSelectors from '../../Redux/Selectors/authSelectors';
+import classnames from "classnames";
 
 
 const UserName = () => {
@@ -12,10 +13,15 @@ const UserName = () => {
   const username = useSelector(authSelectors.getUserName);
   const initials = username[0];
 
+  const [isOpened, setOpened] = useState(false);
+  const toggleIsOpened = () => {
+    setOpened(!isOpened);
+  };
+
   return (
-    <div className={styles["nav-user"]}>
+    <div className={styles["nav-user"]} onClick={toggleIsOpened}>
       {isLoggedIn ? <div className={styles["nav-user-initials"]}>{initials}</div> : null}
-      {isLoggedIn ? <><div className={styles["nav-user-name"]}>{username}</div><div className={styles["nav-user-logout"]}>Log Out</div></> : <NavLink to={PageNames.SignIn}>Sign In</NavLink>}
+      {isLoggedIn ? <><div className={styles["nav-user-name"]}>{username}</div><div className={classnames(styles['nav-user-logout'], {[styles.show]: isOpened})}>Log Out</div></> : <NavLink to={PageNames.SignIn}>Sign In</NavLink>}
     </div>
   );
 };
